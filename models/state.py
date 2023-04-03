5f92ff522666b09f949498ffcb3b83f7ae2432d2
Your Name <you@example.com>
2023-04-03 15:39:12 +0200

    mise a jour

--- models/state.py
+++ models/state.py
@@ -7,15 +7,25 @@ from sqlalchemy.orm import relationship
 from models.city import City
 
 class State(BaseModel, Base):
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
 
-    @property
-    def cities(self):
-        cityList = []
-        cities = storage.all(City)
-        for city in cities.values():
-            if self.id == city.state_id:
-                cityList.append(city)
-        return cityList
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
