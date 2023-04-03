9c9c6cd243fa63dded83e7c01d8d5dc0ec8073f8
Your Name <you@example.com>
2023-04-03 16:09:23 +0200

    mise ajour

--- models/state.py
+++ models/state.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
+"""
+from models import storage
+from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
+"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
