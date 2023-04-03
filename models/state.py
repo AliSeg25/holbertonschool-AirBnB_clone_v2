c8f2d4976858de8e4651afef3396875cfebb7c37
Your Name <you@example.com>
2023-04-03 16:12:14 +0200

    mise ajour

--- models/state.py
+++ models/state.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 """
-
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.city import City
+"""
+from models import storage
+from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
@@ -25,35 +29,6 @@ class State(BaseModel, Base):
         else:
             cityList = []
             all_cities = models.storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    cityList.append(city)
-            return cityList
-"""
-from models import storage
-from models.base_model import BaseModel
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-
-
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="delete")
-
-    @property
-    def cities(self):
-        """returns instances of City"""
-        from models.city import City
-        if storage_t == "db":
-            cityList = []
-            for city in self.cities:
-                cityList.append(city)
-            return cityList
-        else:
-            cityList = []
-            all_cities = storage.all(City)
             for city in all_cities.values():
                 if city.state_id == self.id:
                     cityList.append(city)
