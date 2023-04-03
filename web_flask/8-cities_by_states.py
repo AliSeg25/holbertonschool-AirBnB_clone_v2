314d981b3ee1220ea0b1764f055f2c58adde5b72
Your Name <you@example.com>
2023-04-03 22:06:37 +0200

    mise  ajour

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Module that starts a Flask web application"""
-"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -21,33 +20,3 @@ def close_storage(db):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
-"""
-#!/usr/bin/python3
-"""
-Flask web application
-"""
-from flask import Flask, render_template
-from models import storage
-from models.state import State
-
-
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.teardown_appcontext
-def teardown(exception):
-    """Remove the current SQLAlchemy Session"""
-    storage.close()
-
-
-@app.route('/cities_by_states')
-def display_states_cities():
-    """Display the states and cities sorted by state and city name"""
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
-    return render_template('8-cities_by_states.html', states=states)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
