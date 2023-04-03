f19f7678b0b0bbecc0d151842d51fe04d34b0046
Your Name <you@example.com>
2023-04-03 15:45:57 +0200

    mise a jour

--- models/state.py
+++ models/state.py
@@ -4,7 +4,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-import models
 from models.city import City
 
 class State(BaseModel, Base):
