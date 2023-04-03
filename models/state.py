4405bbe204e784c45b8c8e9c63ce772861ef05bb
Your Name <you@example.com>
2023-04-03 15:47:22 +0200

    mise a jour

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 
+from models import storage
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
