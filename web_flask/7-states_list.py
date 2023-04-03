612cc5701391a5fd2f8db908f931428cf358ee89
Your Name <you@example.com>
2023-04-03 18:01:22 +0200

    mise a jour avec la task7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
+"""Module that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
-
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def states_list():
+def list_state():
     """Displays a list of states in a website"""
-    all_states = storage.all(State)
-    sorted_states = sorted(all_states.values(), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    state_list = storage.all(State).values()
+    return render_template('7-states_list.html', state_list=state_list)
 
 
 @app.teardown_appcontext
-def close_session(exception):
+def close_storage(db):
     """Closes the current SQLAlchemy session"""
     storage.close()
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
