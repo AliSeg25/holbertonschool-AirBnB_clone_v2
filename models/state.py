350b20e54c707cf80a5263a225ad7501b7d71a55
Your Name <you@example.com>
2023-04-03 15:40:53 +0200

    mise a jour

--- models/state.py
+++ models/state.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.city import City
+import models
 
 class State(BaseModel, Base):
     """Representation of state """
