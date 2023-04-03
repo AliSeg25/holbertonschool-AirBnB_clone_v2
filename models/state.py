c902249f91c9da2441fb4336f2adbf81b7de25f2
Your Name <you@example.com>
2023-04-03 15:51:28 +0200

    mise a jour

--- models/state.py
+++ models/state.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-
-from models import storage
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.city import City
 
+
 class State(BaseModel, Base):
-    """Representation of state """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
-        name = ""
+    """ State class """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
 
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
-
-    if models.storage_t != "db":
-        @property
-        def cities(self):
-            """getter for list of city instances related to the state"""
-            city_list = []
+    @property
+    def cities(self):
+        """returns instances of City"""
+        if models.storage_t == "db":
+            cityList = []
+            for city in self.cities:
+                cityList.append(city)
+            return cityList
+        else:
+            cityList = []
             all_cities = models.storage.all(City)
             for city in all_cities.values():
                 if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+                    cityList.append(city)
+            return cityList
