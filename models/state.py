559732e1054f9db8edef756c1e5f51cda284b623
Your Name <you@example.com>
2023-04-03 16:11:15 +0200

    mise ajour

--- models/state.py
+++ models/state.py
@@ -1,16 +1,39 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 """
-from models import storage
-from models.base_model import BaseModel
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from models.city import City
-"""
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.city import City
+
+
+class State(BaseModel, Base):
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    @property
+    def cities(self):
+
+        if models.storage_t == "db":
+            cityList = []
+            for city in self.cities:
+                cityList.append(city)
+            return cityList
+        else:
+            cityList = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    cityList.append(city)
+            return cityList
+"""
+from models import storage
+from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
@@ -22,14 +45,15 @@ class State(BaseModel, Base):
     @property
     def cities(self):
         """returns instances of City"""
-        if models.storage_t == "db":
+        from models.city import City
+        if storage_t == "db":
             cityList = []
             for city in self.cities:
                 cityList.append(city)
             return cityList
         else:
             cityList = []
-            all_cities = models.storage.all(City)
+            all_cities = storage.all(City)
             for city in all_cities.values():
                 if city.state_id == self.id:
                     cityList.append(city)
