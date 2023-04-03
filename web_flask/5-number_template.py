7f0470d6c517c4fe8679be2981606090d75fbb7b
Your Name <you@example.com>
2023-04-03 12:48:21 +0200

    task5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" Module to start a Flask web application """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def print_hello_hbnb():
+    """ return sur la page "Hello HBNB!" """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def print_hbnb():
+    """ return sur la page "HBNB" """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def printctext(text):
+    """ return c url"""
+    return "c {}".format(text.replace('_', ' ')).capitalize()
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def printpython(text):
+    """ return python url"""
+    return "python {}".format(text.replace('_', ' ')).capitalize()
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def print_n(n):
+    """print n """
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def print_number_template(n):
+    """ Display a HTML page only if n is an integer """
+    return """
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>HBNB</title>
+    </head>
+    <body>
+        <h1>Number: {}</h1>
+    </body>
+</html>
+""".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
