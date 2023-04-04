61b3b80e5e786e36d3bd63ecb9eeaa6a1471331e
Your Name <you@example.com>
2023-04-04 10:27:20 +0200

    help

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,1 +1,28 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+"""
+from flask import Flask, render_template
+import sys
+sys.path.append('/home/ali/holbertonschool-AirBnB_clone_v2')
+from models import storage
+from models.state import State
+app = Flask(__name__)
 
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Fetches data from storage engine"""
+    data = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=data)
+
+
+@app.teardown_appcontext
+def close_session(response_or_exc):
+    """Close the current SQLAlchemy session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
