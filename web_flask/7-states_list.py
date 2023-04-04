4eae6da41553800666b019e738281f322d8ef867
Your Name <you@example.com>
2023-04-04 10:21:00 +0200

    mareeee

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
 """
+Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+"""
+from flask import Flask, render_template
+import sys
+sys.path.append('/home/ali/holbertonschool-AirBnB_clone_v2')
 from models import storage
-from flask import Flask
-from flask import render_template
-
+from models.state import State
 app = Flask(__name__)
-
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-    States are sorted by name.
-    """
+    """State objects in DBStorage."""
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+    """Remove session"""
     storage.close()
 
 
