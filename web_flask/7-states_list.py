c9cc0ef73b712ed267630d15055dff2f09bf6cda
Your Name <you@example.com>
2023-04-04 18:21:52 +0200

    task7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.ss
-"""
+"""Flask web application"""
 from flask import Flask, render_template
 import sys
 sys.path.append('/home/ali/holbertonschool-AirBnB_clone_v2')
@@ -11,18 +8,17 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Displays an HTML page containing a list of states"""
-    states = storage.all(State).values()
-    return render_template('7-states_list.html', states=states)
+@app.route('/states', strict_slashes=False)
+def récuperer_donnée():
+    # Utiliser le storage pour récuperer les données
+    state_dict = storage.all(State).values()
+    return render_template('7-states_list.html', state_dict=state_dict)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Removes the current SQLAlchemy Session"""
+def storage_close(db):
+    # fermer la session
     storage.close()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
