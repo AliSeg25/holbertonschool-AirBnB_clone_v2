3200c95ad914a3cd8c0059ba0dc9048067222495
Your Name <you@example.com>
2023-04-04 09:49:21 +0200

    mareeee

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""Flask web application"""
+"""Module that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-def states_list():
-    """Display a list of all State objects sorted by name"""
-    state_list = storage.all(State).order_by(State.name)
+@app.route('/states_list', strict_slashes=False)
+def list_state():
+    """Displays a list of states in a website"""
+    state_list = storage.all(State).values()
     return render_template('7-states_list.html', state_list=state_list)
 
 
 @app.teardown_appcontext
-def close_storage(error):
-    """Close the current SQLAlchemy session"""
+def close_storage(db):
+    """Closes the current SQLAlchemy session"""
     storage.close()
 
 if __name__ == "__main__":
