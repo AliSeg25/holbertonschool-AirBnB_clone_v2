1d1be8d11869534f576cd732100ff3c1cc0cf5d9
Your Name <you@example.com>
2023-04-05 13:43:35 +0200

    task8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,24 +5,25 @@ The application listens on 0.0.0.0, port 5000.
 """
 from flask import Flask, render_template
 import sys
-sys.path.append('/home/ali/holbertonschool-AirBnB_clone_v2')
 from models import storage
 from models.state import State
+
+
+sys.path.append('/home/ali/holbertonschool-AirBnB_clone_v2')
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Fetches data from storage engine"""
-    data = storage.all(State).values()
-    return render_template('8-cities_by_states.html', states=data)
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('8-cities_by_states.html', states=sorted_states)
 
 
 @app.teardown_appcontext
-def close_session(response_or_exc):
-    """Close the current SQLAlchemy session"""
+def storage_close(db):
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
