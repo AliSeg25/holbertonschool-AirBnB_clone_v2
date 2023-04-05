3e2114a44abae323435d1ace187806a8d764c4c5
Your Name <you@example.com>
2023-04-05 13:55:46 +0200

    task8

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,12 @@
 from models.engine.file_storage import FileStorage
 from os import getenv
 from models.engine.db_storage import DBStorage
+from .state import State
+from .city import City
+from .user import User
+from .place import Place
+from .review import Review
+from .amenity import Amenity
 
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
