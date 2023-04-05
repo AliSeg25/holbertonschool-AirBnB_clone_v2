b7296cb0decc955239ed491a200a60d75b4520d5
Your Name <you@example.com>
2023-04-05 19:07:10 +0200

    task

--- models/city.py
+++ models/city.py
@@ -3,16 +3,15 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-
 from sqlalchemy import DateTime
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = "cities"
+    """City class for HBNB project"""
+    __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id", onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
-    id = Column(String(36), primary_key=True, nullable=False, unique=True)
+    state_id = Column(String(36), ForeignKey('states.id'), nullable=False)
+    id = Column(String(36), primary_key=True)
     created_at = Column(DateTime, nullable=False)
     updated_at = Column(DateTime, nullable=False)
     places = relationship('Place', cascade='all, delete', backref='cities', overlaps="cities")
