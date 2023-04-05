cfc2513f29d62f24d7eb94f7517fb7e59a013c5f
Your Name <you@example.com>
2023-04-05 14:01:19 +0200

    task8

--- models/city.py
+++ models/city.py
@@ -13,7 +13,7 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    id = Column(String(36), primary_key=True)
+    state_id = Column(String(60), ForeignKey("states.id", onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
+    id = Column(String(36), primary_key=True, nullable=False, unique=True)
 
     places = relationship('Place', cascade='all, delete', backref='cities', overlaps="cities")
